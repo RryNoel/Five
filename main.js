bff6fa8dcdee622c7066984c9b0fc0f9c1b472d8
JeongSeok <97824309+RryNoel@users.noreply.github.com>
2022-04-12 14:45:31 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+const vertical = document.querySelector('.vertical')
+const horizontal = document.querySelector('.horizontal')
+const target = document.querySelector('.target')
+const tag = document.querySelector('.tag')
+
+addEventListener('load', function() {
+    const targetRect = target.getBoundingClientRect();
+    
+    const targetHalfWidth = targetRect.width / 2;
+    const targetHalfHeight = targetRect.height / 2;
+    
+    document.addEventListener('mousemove', (event) => {
+        const x = event.clientX;
+        const y = event.clientY;
+        console.log(x, y);
+        vertical.style.transform = `translateX(${x}px)`;
+        horizontal.style.transform = `translateY(${y}px)`;
+        target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeight}px)`;
+        tag.style.transform = `translate(${x}px, ${y}px)`;
+        tag.innerHTML = `${x}px ${y}px`;
+    })
+})
