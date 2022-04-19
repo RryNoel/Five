fbf4750d581bd47f2753ab928c187120c6949936
JeongSeok <97824309+RryNoel@users.noreply.github.com>
2022-04-19 15:23:01 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -9,15 +9,28 @@ addEventListener('load', function() {
     
     const targetHalfWidth = targetRect.width / 2;
     const targetHalfHeight = targetRect.height / 2;
+
+    let curX = 0;
+    let curY = 0;
+    let mouseX = 0;
+    let mouseY = 0;
     
-    document.addEventListener('mousemove', (event) => {
-        const x = event.clientX;
-        const y = event.clientY;
+    document.addEventListener('mousemove', (e) => {
+        mouseX = e.clientX;
+        mouseY = e.clientY;
         console.log(x, y);
-        vertical.style.transform = `translateX(${x}px)`;
-        horizontal.style.transform = `translateY(${y}px)`;
-        target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeight}px)`;
-        tag.style.transform = `translate(${x}px, ${y}px)`;
-        tag.innerHTML = `${x}px ${y}px`;
     })
+    
+    function animate() {
+        requestAnimationFrame(animate);
+        curX += (mouseX - curX) * 0.2;
+        curY += (mouseY - curY) * 0.2;
+
+        vertical.style.transform = `translateX(${curX}px)`;
+        horizontal.style.transform = `translateY(${curY}px)`;
+        target.style.transform = `translate(${curX-targetHalfWidth}px, ${curY-targetHalfHeight}px)`;
+        tag.style.transform = `translate(${curX}px, ${curY}px)`;
+        tag.innerHTML = `${Math.ceil(curX)}px ${Math.ceil(curY)}px`;
+    }
+    animate();
 })
